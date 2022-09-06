3f7558c22edb30cfda32449ecab1ae46072882b0
ESTOLEL <elijah.alixtair.estolas@oocl.com>
2022-09-06 17:30:24 +0800

    Feat: added findByGender

--- src/main/java/com/rest/springbootemployee/EmployeeRepository.java
+++ src/main/java/com/rest/springbootemployee/EmployeeRepository.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Repository
 public class EmployeeRepository {
@@ -14,19 +15,26 @@ public class EmployeeRepository {
     public EmployeeRepository() {
         employees = new ArrayList<>();
         employees.add(new Employee(1, "Jep", 23, "Male", 1000));
+        employees.add(new Employee(1, "Jennifer", 23, "Female", 1000));
+        employees.add(new Employee(1, "Ruby", 23, "Female", 1000));
     }
-    public List<Employee> findAll () {
+
+    public List<Employee> findAll() {
         return employees;
     }
 
-    public Employee findById (Integer employeeID) {
+    public Employee findById(Integer employeeID) {
         return employees.stream()
                 .filter(employee -> employee.getEmployeeID() == employeeID.intValue())
                 .findFirst()
                 .orElseThrow(NoEmployeeFoundException::new);
     }
 
-    public List<Employee> findByGender (String gender) {
-        return null;
+    public List<Employee> findByGender(String gender) {
+        return employees.stream()
+                .filter(employee -> {
+                    return employee.getGender().equals(gender);
+                })
+                .collect(Collectors.toList());
     }
 }
